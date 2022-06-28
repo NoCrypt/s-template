614057b6503ca81609f81c15a188ec807dc2bdff
NoCrypt <aldyhkmz@gmail.com>
2022-06-28 16:55:21 +0700

    fix responsiveness

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,16 +9,20 @@
 	});
 </script>
 
-<div class="hero min-h-screen bg-base-200">
-	<div class="hero-content p-4 text-center">
-		<div class="max-w-md">
+<svelte:head>
+  <title>Svelte template</title>
+  <meta name="description" content="My personal sveltekit template"/>
+</svelte:head>
+
+<div class="flex min-h-screen bg-base-200 p-5 text-center justify-center items-center">
+		<div class="max-w-md w-full">
       <pre class="text-8xl pb-6 pointer-events-none select-none">🎉</pre>
 			<h1 class="text-5xl font-bold">Hey, It works!</h1>
 			<p class="py-6">
 				My svelte template for rapidly make web apps. It's a sveltekit template that have many things configured, like tailwindcss, 
         theme-change, daisyUI, rubik fonts from fontsource, no tests out-of-the-box (add ur own), less boilerplate. Ready to deploy anywhere without any extra config.
 			</p>
-			<div class="mockup-code text-left mb-6 ">
+			<div class="mockup-code text-left mb-6 min-w-0">
 				<pre data-prefix="$"><code>npx degit NoCrypt/s-template {"<APP_NAME>"} </code></pre>
         <pre data-prefix="$"><code>cd {"<APP_NAME>"}</code></pre>
         <pre data-prefix="$"><code>pnpm i && code .</code></pre>
@@ -27,5 +31,4 @@
 				>Change Theme</button
 			>
 		</div>
-	</div>
 </div>
